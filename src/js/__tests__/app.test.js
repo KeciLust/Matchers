6e92d36969103271b46ccda79785ff0c5d79dbeb
Denis Nikitin <dinya.nikitin@gmail.com>
2021-06-27 22:04:16 +0300

    change test

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -3,21 +3,20 @@ import comparisonHealth from '../app';
 const item = [
   { name: 'лучник', health: 80 },
   { name: 'маг', health: 100 },
-  { name: 'лучник', health: 80 },
+  { name: 'мечник', health: 10 },
 ];
 test('comparison', () => {
   const received = comparisonHealth(item);
   expect(received).toEqual([
     { name: 'маг', health: 100 },
     { name: 'лучник', health: 80 },
-    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ]);
 });
-// test('com', () => {
-//   const received = comparisonHealth(item);
-//   expect(received).toBe([
-//     { name: 'маг', health: 100 },
-//     { name: 'лучник', health: 80 },
-//     { name: 'лучник', health: 80 },
-//   ]);
-// });
+test('com', () => {
+  const received = comparisonHealth(item);
+  expect(received).toBe([{ name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ]);
+});
